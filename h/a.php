039ac9d2c9e52020e2028875edad4431f3d91a93
4102-cd <66010914102@msu.ac.th>
2026-02-09 23:10:38 +0700

    Add files via upload

---
+++ h/a.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ชัชวาล วุฑฒะกุล (เตอร์) </title>
+</head>
+
+<body>
+<h1> a.php</h1>
+
+<?php
+	$_SESSION['name']="ชัชวาล วุฑฒะกุล  ";
+	$_SESSION['nickname']=" เตอร์";
+	//echo $_SESSION['name'];	
+	//echo $_SESSION['nickname'];
+?>
+</body>
+</html>
